c4d1aca863397a79f3dcc4a53a96a6d6305331e4
Gopinath K <gopinath_k@hotmail.com>
2020-10-16 11:13:08 +0530

    issue#2: modified One.java by branch2

--- One.java
+++ One.java
@@ -1,5 +1,6 @@
 public class One{
 	public static void main(String args[]){
 		System.out.println("from one.java class file");
+		System.out.println("from one.java class file .... added by branch2");
 	}
 }
