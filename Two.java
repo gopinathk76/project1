dec46a193512393042f0a0600c6db997a56dadf4
Gopinath K <gopinath_k@hotmail.com>
2021-01-22 15:00:48 +0530

    added a sop line to the version gk/issue2/2

--- Two.java
+++ Two.java
@@ -1,6 +1,7 @@
 public class Two{
 	public static void main(String args[]){
 		System.out.println("from two.java class ------- modified by master after branch1 was created ----- file");
+		System.out.println("added this line to the branch gk/issue2/2....");
                 
 	}
 }
