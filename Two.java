bd3fd1a376ba6b9a4f713f172c7156eee0b82068
Gopinath K <gopinath_k@hotmail.com>
2021-01-22 14:43:05 +0530

    made changes to Two.java and added to new branch from_gk/issue2/2

--- Two.java
+++ Two.java
@@ -1,6 +1,7 @@
 public class Two{
 	public static void main(String args[]){
 		System.out.println("from two.java class ------- modified by master after branch1 was created ----- file");
+		System.out.println("created a branch from gk/issue2/2...");
                 
 	}
 }
